c138847131fb34b8035aafc22dae0abf42331661
OKY <okyaneka@gmail.com>
2024-03-04 20:46:01 +0700

    feat(pwa): init pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
+import { VitePWA } from 'vite-plugin-pwa'
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -11,57 +12,106 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import packageJson from './package.json'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/sinau-jp',
-  define: {
-    APP_VERSION: JSON.stringify(packageJson.version),
-    APP_NAME: JSON.stringify(packageJson.name)
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      dts: 'src/autoimport.d.ts',
-      include: [
-        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue$/,
-        /\.vue\?vue/, // .vue
-        /\.md$/ // .md
-      ],
-      imports: [
-        'vue',
-        'vue-router',
-        '@vueuse/core',
-        'pinia',
-        {
-          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
+export default ({ mode }: { mode: string }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd(), ''))
+  const APP_NAME = JSON.stringify(packageJson.name)
+  const APP_VERSION = JSON.stringify(packageJson.version)
+
+  return defineConfig({
+    // base: '/sinau-jp',
+    define: {
+      APP_VERSION,
+      APP_NAME
+    },
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        injectRegister: 'auto',
+        manifest: {
+          screenshots: [
+            {
+              src: 'img/app.png',
+              type: 'image/png',
+              sizes: '1080x1080',
+              form_factor: 'narrow'
+            },
+            {
+              src: 'img/app.png',
+              type: 'image/png',
+              sizes: '1080x1080',
+              form_factor: 'wide'
+            }
+          ],
+          name: process.env.VITE_APP_NAME,
+          short_name: APP_NAME,
+          description: 'My Awesome App description',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'img/icons/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'img/icons/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        },
+        devOptions: {
+          enabled: true
+        },
+        workbox: {
+          clientsClaim: true,
+          skipWaiting: true
         }
-      ],
-      eslintrc: { enabled: true },
-      resolvers: [NaiveUiResolver()],
-      dirs: ['src/models', 'src/middlewares', 'src/plugins']
-    }),
-    Components({
-      dts: 'src/components.d.ts',
-      extensions: ['vue', 'md'],
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dirs: ['src/components'],
-      directoryAsNamespace: true,
-      resolvers: [NaiveUiResolver(), IconsResolver()]
-    }),
-    Icons({
-      autoInstall: true,
-      compiler: 'vue3'
-    }),
-    Pages({
-      dirs: [{ baseRoute: '', dir: 'src/views' }]
-    }),
-    Layouts({
-      layoutsDirs: 'src/layouts'
-    })
-  ],
-  resolve: {
-    alias: {
-      '~': fileURLToPath(new URL('./src', import.meta.url))
+      }),
+      vue(),
+      AutoImport({
+        dts: 'src/autoimport.d.ts',
+        include: [
+          /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+          /\.vue$/,
+          /\.vue\?vue/, // .vue
+          /\.md$/ // .md
+        ],
+        imports: [
+          'vue',
+          'vue-router',
+          '@vueuse/core',
+          'pinia',
+          {
+            'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
+          }
+        ],
+        eslintrc: { enabled: true },
+        resolvers: [NaiveUiResolver()],
+        dirs: ['src/models', 'src/middlewares', 'src/plugins']
+      }),
+      Components({
+        dts: 'src/components.d.ts',
+        extensions: ['vue', 'md'],
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        dirs: ['src/components'],
+        directoryAsNamespace: true,
+        resolvers: [NaiveUiResolver(), IconsResolver()]
+      }),
+      Icons({
+        autoInstall: true,
+        compiler: 'vue3'
+      }),
+      Pages({
+        dirs: [{ baseRoute: '', dir: 'src/views' }]
+      }),
+      Layouts({
+        layoutsDirs: 'src/layouts'
+      })
+    ],
+    resolve: {
+      alias: {
+        '~': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
-})
+  })
+}
