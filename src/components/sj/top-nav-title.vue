067893d8fb5ddbe7e0564307effc170ea7e8a600
OKY <okyaneka@gmail.com>
2024-06-19 16:18:37 +0700

    feat(guide): add guide which contain hiragana and katakana

---
+++ src/components/sj/top-nav-title.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { RouteLocationRaw } from 'vue-router'
+import { config } from '~/global/config'
+
+defineProps<{
+  to: RouteLocationRaw
+}>()
+</script>
+
+<template>
+  <n-card class="fixed top-0 z-50" content-class="px-4 h-14 py-0 flex-none flex items-center">
+    <div class="flex-1 text-center font-semibold">{{ config.title }}</div>
+  </n-card>
+  <div class="mb-15"></div>
+</template>
--- src/global/config.ts
+++ src/global/config.ts
@@ -1,3 +1,5 @@
+export const config = { title: import.meta.env.VITE_APP_NAME }
+
 export function title(title: string) {
-  return `${title ? title + ' - ' : ''} ${import.meta.env.VITE_APP_NAME}`.trim()
+  return `${title ? title + ' - ' : ''} ${config.title}`.trim()
 }
